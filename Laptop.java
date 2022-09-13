46346e3d095bbcb3a8a44d27fe7afad49e300844
chethan A naik <spoorthignaikvs.xworkz@gmail.com>
2022-09-13 10:15:01 +0530

    first program

---
+++ Laptop.java
@@ -0,0 +1,27 @@
+class  Laptop{
+	
+	public static void main (String[] a){
+	System.out.println("Laptop");
+	
+	int  RAM =12;
+	short  laptopprice=31000;
+	boolean fast=true;
+	
+	System.out.println("ram= "+12);
+	System.out.println("Laptop price="+32000);
+	System.out.println("fast="+true);
+			
+	
+	int rom=24;
+	float  storage =128;
+	boolean updatedversion=false;
+	
+	
+	System.out.println("rom= "+24);
+	System.out.println("storage= "+28);
+	System.out.println(" updated version="+false);
+	
+
+	}
+	
+}
