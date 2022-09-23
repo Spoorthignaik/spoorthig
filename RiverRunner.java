00cc17417dec13cb4f39f1a79860175765c97a63
Spoorthignaik <113289638+Spoorthignaik@users.noreply.github.com>
2022-09-23 08:07:23 +0530

    23/09

---
+++ RiverRunner.java
@@ -0,0 +1,43 @@
+class RiverRunner{
+	
+	public static void main(String[] values){
+	
+	System.out.println("enter");
+	
+	String ref=values[0];
+	System.out.println("index 0:"+ref);
+	
+	String ref1=values[1];
+	System.out.println("index 1:"+ref1);
+	
+	String ref2=values[2];
+	System.out.println("index 2:"+ref2);
+	
+	String ref3=values[3];
+	System.out.println("index 3:"+ref3);
+	
+	String ref4=values[4];
+	System.out.println("index 4:"+ref4);
+	
+	String ref5=values[5];
+	System.out.println("index 5:"+ref5);
+	
+	String ref6=values[6];
+	System.out.println("index 6:"+ref6);
+	
+	String ref7=values[7];
+	System.out.println("index 7:"+ref7);
+	
+	String ref8=values[8];
+	System.out.println("index 8:"+ref8);
+	
+	String ref9=values[9];
+	System.out.println("index 9:"+ref9);
+
+	
+	System.out.println("exit");
+	
+	
+	}
+	
+}
